fc1e1f6526fbdd94258c5599a5f9941a99ce2da9
Deon <steenkampdm2006@gmail.com>
2026-08-31 15:20:00 +0200

    fixed some parts of the component of the composite pattern

--- EventComponent.cpp
+++ EventComponent.cpp
@@ -1,8 +1,8 @@
 #include "EventComponent.h"
-
 using namespace std;
+
 EventComponent::EventComponent(string name) : name_(name) {}
 
-const std::string& EventComponent::getName() const {
+const string& EventComponent::getName() const {
     return name_;
 }
